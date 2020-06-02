7e9480907849a9613d463c2b04504dda1e33f6f3
Carlos Mertens <mertenscarlos@yahoo.com>
2020-06-02 23:53:25 +0200

    Render component inline Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./navbar/Navbar";
+import Home from "./Home";
 
-const Home = () => <h1>Home</h1>;
 const Host = () => <h1>Host</h1>;
 const Help = () => <h1>Help</h1>;
 const SignUp = () => <h1>Sign Up</h1>;
@@ -14,7 +14,13 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Route exact path="/" component={Home} />
+      <Route
+        exact
+        path="/"
+        render={() => {
+          return <Home title="Welcome Home!" />;
+        }}
+      />
       <Route exact path="/host" component={Host} />
       <Route exact path="/help" component={Help} />
       <Route exact path="/signup" component={SignUp} />
