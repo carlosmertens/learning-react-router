a97c9afd234d3b4a79884c67d21f03367cf71c68
Carlos Mertens <mertenscarlos@yahoo.com>
2020-06-02 22:58:47 +0200

    Test basic use of Router, Route and Link component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <h1>HEADER</h1>
+      <Link to="/">Home</Link>
+      <br />
+      <Link to="/about">About</Link>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <h1>FOOTER</h1>
+    </Router>
   );
 }
 
